d498e861e99993ea696ad927875f82c85f3bd113
MBlml <juan.barajas5570@alumnos.udg.mx>
2025-04-05 16:28:05 -0600

    New files

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const params = new URLSearchParams(window.location.search);
+const movieId = params.get("movie");
+
+// Diccionario de iframes completos por ID
+const iframes = {
+  1: `<iframe src="https://cybervynx.com/e/wtecc4h2vzp8" 
+          frameborder="0" 
+          marginwidth="0" 
+          marginheight="0" 
+          scrolling="no"
+          width=640 
+          height=360
+          allowfullscreen></iframe>`,
+  2: `<iframe src="https://cybervynx.com/e/mev5ty8vlk61" 
+          frameborder="0" 
+          marginwidth="0" 
+          marginheight="0" 
+          scrolling="no"
+          width=640 
+          height=360 
+          allowfullscreen></iframe>`,
+  4: ``,
+  5: ``,
+  6: ``,
+  7: ``,
+  8: ``,
+  9: ``,
+  10: ``,
+};
+
+const container = document.getElementById("iframe-container");
+
+if (movieId && iframes[movieId]) {
+  container.innerHTML = iframes[movieId];
+} else {
+  container.innerHTML = "<p style='text-align:center;'>Película no disponible.</p>";
+}
